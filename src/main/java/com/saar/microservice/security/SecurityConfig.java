a6c5ad7a491c41ded6f0bfd41ebc87e9512b19c3
Saar <saar@ox-dynamics.com>
2025-03-30 00:08:33 +0300

    change spring security 6

--- src/main/java/com/saar/microservice/security/SecurityConfig.java
+++ src/main/java/com/saar/microservice/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.saar.microservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     @Bean(name = "regularFilterChain")
