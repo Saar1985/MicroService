dbe27c2b35a0dedfea85ef4a394f0f49324c75b8
Saar <saar@ox-dynamics.com>
2025-03-29 23:16:55 +0300

    change spring security 4

--- src/test/java/com/saar/microservice/MicroServiceApplicationTests.java
+++ src/test/java/com/saar/microservice/MicroServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.saar.microservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"})
+@ActiveProfiles("test")
 class MicroServiceApplicationTests {
 
     @Test
