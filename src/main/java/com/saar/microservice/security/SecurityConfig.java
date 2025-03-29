838d3f0ad5cbe2cb8807807284682e704f9e3929
Saar <saar@ox-dynamics.com>
2025-03-30 00:20:52 +0300

    change spring security 7

--- src/main/java/com/saar/microservice/security/SecurityConfig.java
+++ src/main/java/com/saar/microservice/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.saar.microservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,19 +11,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@Profile("!test")
 public class SecurityConfig {
 
-    @Bean(name = "regularFilterChain")
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/actuator/**", "/users/**","/swagger-ui/**", "/v3/api-docs/**" ).permitAll()  // Allow access to these endpoints without authentication
-                )
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-                .httpBasic(withDefaults())  // Enable HTTP Basic Authentication
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Allow all requests
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity in tests
                 .build();
     }
 }
