02beb71b65742a4ccfe0edcb18a1dd95d8419418
Saar <saar@ox-dynamics.com>
2025-03-29 23:04:08 +0300

    change spring security 3

--- src/main/java/com/saar/microservice/security/SecurityConfig.java
+++ src/main/java/com/saar/microservice/security/SecurityConfig.java
@@ -2,11 +2,11 @@ package com.saar.microservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -16,9 +16,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/actuator/**", "/users/**","/swagger-ui/**", "/v3/api-docs/**" ).permitAll()  // Allow access to these endpoints without authentication
-                        .anyRequest().authenticated()  // Secure other endpoints with authentication
+                )
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
                 .httpBasic(withDefaults())  // Enable HTTP Basic Authentication
                 .build();
