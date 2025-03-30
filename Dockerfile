25d442af440e17ae874603926b4c5c3ca29c1333
Saar <saar@ox-dynamics.com>
2025-03-30 18:23:04 +0300

    Create spring boot profiles

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM eclipse-temurin:17-jdk
 WORKDIR /app
 COPY target/*.jar app.jar
+# Expose the port your application runs on
+EXPOSE 8080
+# Set the Spring profile to production
+ENV SPRING_PROFILES_ACTIVE=prod
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/com/saar/microservice/MicroServiceApplicationTests.java
+++ src/test/java/com/saar/microservice/MicroServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.saar.microservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 
-@SpringBootTest(properties = {"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"})
+@SpringBootTest
 class MicroServiceApplicationTests {
 
     @Test
